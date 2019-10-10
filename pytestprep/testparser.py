e72508a3312cfa1d3538b433c2b6ab3c70671ec5
Tom English <redsoxfantom@gmail.com>
2019-10-10 19:44:29 -0400

    Bug fixes for question parsing

--- pytestprep/testparser.py
+++ pytestprep/testparser.py
@@ -4,10 +4,12 @@ import re
 
 class Question:
     def __init__(self,question,choices,answer,explanation):
-        self.question = question
-        self.choices = choices
-        self.answer = answer
-        self.explanation = explanation
+        self.question = question.strip()
+        self.choices = {}
+        for choice in choices:
+            self.choices[choice] = choices[choice].strip()
+        self.answer = answer.strip()
+        self.explanation = explanation.strip()
 
 def parse_test_bank(file):
     question_regex = r"QUESTION NO: \d+"
@@ -29,27 +31,31 @@ def parse_test_bank(file):
         question_match = re.search(question_regex,text)
         option_match = re.search(option_regex,text)
         answer_match = re.search(answer_regex,text)
-        if in_question:
-            question_text = question_text + "\n"+ text
-        if in_option:
-            options[current_option] = text
-        if in_explanation:
+        if in_explanation and not question_match:
             explanation_text = explanation_text + "\n" + text
+        if in_option and not option_match and not answer_match:
+            options[current_option] = options[current_option] + "\n" + text
+        if in_question and not option_match:
+            question_text = question_text + "\n"+ text
+        if answer_match:
+            in_question = False
+            in_explanation = True
+            in_option = False
+            answer = answer_match.group(1)
+        if option_match:
+            in_question = False
+            in_explanation = False
+            in_option = True
+            current_option = text
+            options[current_option] = ""
         if question_match:
             in_question = True
             in_explanation = False
             in_option = False
             if answer:
                 questions.append(Question(question_text,options,answer,explanation_text))
+            answer = None
             options = {}
-        if option_match:
-            in_question = False
-            in_explanation = False
-            in_option = True
-            current_option = text
-        if answer_match:
-            in_question = False
-            in_explanation = True
-            in_option = False
-            answer = answer_match.group(1)
+            question_text = ""
+            explanation_text = ""
     return questions
--- tester.py
+++ tester.py
@@ -1,7 +1,18 @@
 from pytestprep.testparser import parse_test_bank
 import sys
+from pprint import pprint
+
+def print_question(q):
+    print(q.question)
+    pprint(q.choices)
+    print(q.answer)
+    print(q.explanation)
+
 
 testfile = sys.argv[1]
 print("Parsing %s..." % testfile)
-parse_test_bank(testfile)
+questions = parse_test_bank(testfile)
+print("Parsed %s questions from %s" % (len(questions),testfile))
 
+for q in questions:
+    print_question(q)
